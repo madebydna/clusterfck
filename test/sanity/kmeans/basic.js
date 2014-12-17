4d3d0413d89f60f1417d11fffe4092977b4d993b
Andrea Singh <info@madebydna.com>
2014-12-16 19:16:04 -0800

    Ability to handle keyed data points (array of objects with key and value)

--- test/sanity/kmeans/basic.js
+++ test/sanity/kmeans/basic.js
@@ -11,6 +11,17 @@ var data = [
    [200, 200, 200]
 ];
 
+
+var data_with_objects = [
+    {key: 'a', value: [1, 1, 1]},
+    {key: 'b', value: [2, 2, 2]},
+    {key: 'c', value: [3, 3, 3]},
+    {key: 'd', value: [4, 4, 4]},
+    {key: 'e', value: [5, 5, 5]},
+    {key: 'f', value: [20, 20, 20]},
+    {key: 'g', value: [200, 200, 200]}
+];
+
 var iterations = 20;
 
 exports.testTwoClusters = function(test) {
@@ -51,6 +62,17 @@ exports.testThreeClusters = function(test) {
             [200, 200, 200]
          ]
       ]), "clustered into correct three clusters");
+   }
+   test.done(); 
+}
+
+exports.testTwoClustersWithObjectData = function(test) {
+   for (var i = 0; i < iterations; i++) {
+      var clusters = clusterfck.kmeans(data_with_objects, 2);
+      test.ok(equality.members(clusters, [
+         [ 'a', 'b', 'c', 'd', 'e', 'f' ],
+         [ 'g' ]
+      ]), "clustered into correct two clusters");
    }
    test.done(); 
 }
